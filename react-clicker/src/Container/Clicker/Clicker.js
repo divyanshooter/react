ebe3caff3269e21c390c9fb5e6adf56efb2a4431
divyanshooter <divyanshooter@gmail.com>
2019-08-26 16:55:56 +0530

    EventHandlers Added and Hover Effect is Added

--- react-clicker/src/Container/Clicker/Clicker.js
+++ react-clicker/src/Container/Clicker/Clicker.js
@@ -2,10 +2,27 @@ import React,{Component} from 'react';
 import classes from './Clicker.module.css';
 import Button from '../../Components/UI/Button/Button';
 
+
 class Clicker extends Component
 {
     state={
         count:0
+    }
+
+    addEventHandler=()=>{
+        let oldCount=this.state.count;
+        oldCount=oldCount+1;
+        this.setState({count:oldCount});
+    }
+    minusEventHandler=()=>{
+        let oldCount=this.state.count;
+        oldCount=oldCount-1;
+        this.setState({count:oldCount});
+    }
+    restartEventHandler=()=>{
+        let oldCount=this.state.count;
+        oldCount=0;
+        this.setState({count:oldCount});
     }
     render()
   {
@@ -16,9 +33,9 @@ class Clicker extends Component
               {this.state.count}
               </p>
              <div className={classes.control}>
-             <Button type="add"/>
-             <Button type="restart"/>
-             <Button type="minus"/>
+             <Button type="add" clicked={this.addEventHandler}/>
+             <Button type="restart" clicked={this.restartEventHandler}/>
+             <Button type="minus" clicked={this.minusEventHandler}/>
               
              </div>
              
