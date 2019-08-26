29f2df6891be30e1db354a0038a2cf9350c24579
divyanshooter <divyanshooter@gmail.com>
2019-08-27 00:05:45 +0530

    Hour Updated

--- react-clock/src/Components/Clock/Clock.js
+++ react-clock/src/Components/Clock/Clock.js
@@ -4,7 +4,7 @@ import classes from './Clock.module.css';
 const Clock=(props)=>{
     let day=['Sunday','Monday','Tuesday','Wednesday','Thurday','Friday','Saturday'];
     let Month=['January','Feburary','March','April','May','June','July','August','September','October','November','December'];
-    let time=props.date.getHours()+':'+props.date.getMinutes()+':'+props.date.getSeconds();
+    let time=(props.date.getHours()==0 ? '00' : props.date.getHours())+':'+props.date.getMinutes()+':'+props.date.getSeconds();
     let date=day[props.date.getDay()]+ ' ' +props.date.getDate()+ ' ' +Month[props.date.getMonth()]+ ' ' +props.date.getFullYear();
     return (
         <div className={classes.main}>
